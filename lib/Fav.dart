ac7fab57411b718b3c1e41916bca63aac35e8aa3
Kenert Karu <karukenert@gmail.com>
2021-02-01 22:09:18 +0200

    initial layout and tab logic done

---
+++ lib/Fav.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavTab extends StatefulWidget {
+  @override
+  _FavTabState createState() => _FavTabState();
+}
+
+class _FavTabState extends State<FavTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
