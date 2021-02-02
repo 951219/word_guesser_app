2eb71fa11aefe62d335df16514f3ad3de2e090b8
Kenert Karu <karukenert@gmail.com>
2021-02-02 22:31:19 +0200

    user stays logged in if the app is closed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:word_guesser_app/entry_page.dart';
 import 'package:word_guesser_app/login_page.dart';
 
-void main() {
+// void main() {
+//   runApp(
+//     MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         theme: ThemeData(primaryColor: Colors.black),
+//         home: LoginPage()),
+//   );
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+  var refreshToken = sharedPreferences.getString('refreshToken');
+  //TODO maybe use accesstoken instead and check if token is valid.
   runApp(
     MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primaryColor: Colors.black),
-        home: LoginPage()),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.black),
+      home: refreshToken == null ? LoginPage() : EntryPage(),
+    ),
   );
 }
